54834997667082db83b2e0b09f42d45b1f2d4e82
andrewindeche <indecheandrew@gmail.com>
2026-01-29 11:33:05 +0300

    update controller

--- Controllers/WebhookControllers.cs
+++ Controllers/WebhookControllers.cs
@@ -1,26 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 
+public interface IPaymentEvent
+{
+    string Status { get; set; }
+    int CustomerId { get; set; }
+}
+
+public class PaymentEvent : IPaymentEvent
+{
+    public required string Status { get; set; }
+    public int CustomerId { get; set; }
+}
+
 [ApiController]
 [Route("api/[controller]")]
 public class WebhookController : ControllerBase
 {
-    private readonly IUserRepository _users;
+    private readonly IUserRepository users;
 
     public WebhookController(IUserRepository users)
     {
-        _users = users;
+        this.users = users;
     }
-
+    
     [HttpPost("flutterwave")]
     public IActionResult HandleEvent([FromBody] PaymentEvent evt)
     {
         if (evt.Status == "successful")
         {
-            var user = _users.GetById(evt.CustomerId);
+            var user = users.GetById(evt.CustomerId.ToString());
             if (user != null)
             {
                 user.IsPremium = true;
-                _users.Update(user);
+                users.Update(user);
             }
         }
 
