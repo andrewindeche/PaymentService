5bbbe1691e03262c2aeb4005b34bbf055b6937d1
andrewindeche <indecheandrew@gmail.com>
2026-01-30 13:23:36 +0300

    define connection string

--- Program.cs
+++ Program.cs
@@ -1,54 +1,69 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.Data.SqlClient;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {   
-        options.RequireHttpsMetadata = false;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
-            ),
-            ClockSkew = TimeSpan.Zero 
-        };
-    });
-
-builder.Services.AddHttpClient<FlutterwaveSandboxService>();
-builder.Services.AddHttpClient<AfricasTalkingService>();
-builder.Services.AddAuthorization();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.MapOpenApi();
+    private static void Main(string[] args)
+    {
+        string connectionString = "Server=localhost;Database=MyDatabase;Trusted_Connection=True;";
+
+        using (SqlConnection conn = new(connectionString)) 
+            { 
+                conn.Open(); 
+                Console.WriteLine("Connected successfully!"); 
+            }
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                    ),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+        builder.Services.AddHttpClient<FlutterwaveSandboxService>();
+        builder.Services.AddHttpClient<AfricasTalkingService>();
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapGet("/health", () => "OK");
+
+        app.MapGet("/secure", () => "You are authenticated!")
+           .RequireAuthorization();
+
+        app.MapControllers();
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapGet("/health", () => "OK");
-
-app.MapGet("/secure", () => "You are authenticated!")
-   .RequireAuthorization();
-
-app.MapControllers();
-app.Run();
